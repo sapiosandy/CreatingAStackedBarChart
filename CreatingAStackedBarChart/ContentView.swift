29b989b135597302a08d2ce4ac407ee43d36085b
Sandy Gomez <sgomez1029@gmail.com>
2024-04-10 11:59:56 -0700

    Creating a Stacked Bar Chart

--- CreatingAStackedBarChart/ContentView.swift
+++ CreatingAStackedBarChart/ContentView.swift
@@ -6,16 +6,35 @@
 //
 
 import SwiftUI
+import Charts
+
+struct SalesPeople: Identifiable {
+    var name: String
+    var itemSold: String
+    var sales: Int
+    var id = UUID()
+}
+
+var mySalesArray: [SalesPeople] = [
+    .init(name: "Oscar", itemSold: "Car", sales: 50),
+    .init(name: "Oscar", itemSold: "Truck", sales: 43),
+    .init(name: "Oscar", itemSold: "Loan", sales: 62),
+    .init(name: "Fran", itemSold: "Car", sales: 26),
+    .init(name: "Fran", itemSold: "Truck", sales: 67),
+    .init(name: "Fran", itemSold: "Loan", sales: 38),
+    .init(name: "Billy", itemSold: "Car", sales: 40),
+    .init(name: "Billy", itemSold: "Truck", sales: 82),
+    .init(name: "Billy", itemSold: "Loan", sales: 24)
+]
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Chart(mySalesArray) { x in
+                BarMark(x: .value("Name", x.name),
+                         y: .value("Sales", x.sales))
+                .foregroundStyle(by: .value("Items Sold", x.itemSold))}
         }
-        .padding()
     }
 }
 
