0a27aadeb0619a9aa807f4b2d952b065cafa2e97
Sandy Gomez <sgomez1029@gmail.com>
2024-04-10 11:17:58 -0700

    Initial Commit

---
+++ CreatingAStackedBarChart/CreatingAStackedBarChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreatingAStackedBarChartApp.swift
+//  CreatingAStackedBarChart
+//
+//  Created by Sandra Gomez on 4/10/24.
+//
+
+import SwiftUI
+
+@main
+struct CreatingAStackedBarChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
